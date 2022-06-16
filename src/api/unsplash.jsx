92dc2fb599a42b6c657ad933302ce0b8503105d3
Karen Cameron <karen.k.cameron@gmail.com>
2022-06-16 00:03:03 -0700

    API Key Updated

--- src/api/unsplash.jsx
+++ src/api/unsplash.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
+const apiKey =process.env.REACT_APP_KEY
 
 export default axios.create({
     baseUrl: "https://api.unsplash.com/",
     headers: {
-        Authorization: "Client-ID 1H20HiEh06x6VFJU5eMp1uAunXvXx5Z44jCRTHFCeoQ"
+        Authorization: "apiKey"
     },
 })
